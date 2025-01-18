cf5fd4adaa9c8b9fb0b296ea004721b94b340239
findr247 <finduil247@gmail.com>
2025-01-18 13:03:26 +0100

    first commit

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 # build.sh
 
+# Update package lists and install SQLite development libraries
+apt-get update && apt-get install -y sqlite3 libsqlite3-dev
+
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py

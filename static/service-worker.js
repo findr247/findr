4bb0da3e0e35df14e4b803891c72b3a76cba7d30
findr247 <finduil247@gmail.com>
2025-01-30 13:33:38 +0100

    app setup

---
+++ static/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "assets/css/main.css", // Replace with your CSS file paths
+  "assets/js/main.js", // Replace with your JS file paths
+  "/favicon-96x96.png",
+  "/static/apple-touch-icon.png",
+  "favicon.ico",
+  "favicon.svg",
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
